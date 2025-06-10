4ec8a8af4ddb95c13b5c01a2a0ec99a43461d3bc
Eyasu <eyasugetaneh0@gmail.com>
2025-06-10 23:06:04 +0300

    feat: update code structure and add methods

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,35 +1,39 @@
-# scripts/scraper.py
-
-from google_play_scraper import Sort, reviews
+from google_play_scraper import Sort, reviews_all
 import pandas as pd
-from datetime import datetime
 
-apps = {
-    "Commercial Bank of Ethiopia": "com.combanketh",
-    "Bank of Abyssinia": "com.boa.boamobile",
-    "Dashen Bank": "com.dashen.bank",
+banks = {
+    "CBE": "com.combanketh.mobilebanking",
+    "Awash": "com.sc.awashpa",
+    "Dashen": "com.dashen.dashensuperapp"
 }
 
 all_reviews = []
 
-for bank, app_id in apps.items():
-    result, _ = reviews(
-        app_id,
-        lang='en',
-        country='us',
-        sort=Sort.NEWEST,
-        count=450  # target more to clean up later
-    )
-    for entry in result:
-        all_reviews.append({
-            "review": entry['content'],
-            "rating": entry['score'],
-            "date": entry['at'].strftime("%Y-%m-%d"),
-            "bank": bank,
-            "source": "Google Play"
-        })
+for bank, app_id in banks.items():
+    print(f"Scraping {bank}...")
+    try:
+        reviews = reviews_all(
+            app_id,
+            sleep_milliseconds=0,
+            lang='en',
+            country='us',
+            sort=Sort.NEWEST,
+            count=450
+        )
+        for r in reviews[:500]:
+            all_reviews.append({
+                "review": r['content'],
+                "rating": r['score'],
+                "date": r['at'].date().isoformat(),
+                "bank": bank,
+                "source": "Google Play"
+            })
+    except Exception as e:
+        print(f"Error scraping {bank}: {e}")
 
 df = pd.DataFrame(all_reviews)
+print(f"Total reviews scraped: {len(df)}")
+print(f"Total reviews collected: {len(all_reviews)}")
 
 # Save raw data
 df.to_csv("data/raw_reviews.csv", index=False)
